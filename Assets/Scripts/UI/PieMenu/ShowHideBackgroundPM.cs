241a6491a95a8b6041178645d4da92a46b9a59ed
Xentios <xenq@hotmail.com>
2025-07-20 00:25:12 +0300

    DoTween

--- Assets/Scripts/UI/PieMenu/ShowHideBackgroundPM.cs
+++ Assets/Scripts/UI/PieMenu/ShowHideBackgroundPM.cs
@@ -1,13 +1,32 @@
 using SimplePieMenu;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowHideBackgroundPM : MonoBehaviour, IMenuItemClickHandler
 {
     [SerializeField]
+    private bool workWithInvisableBackground = false;
+    [SerializeField]
     private GameObject background;
+
+    [SerializeField]
+    private Image backgroundImage;
+
+    private bool isVisible = true;
     public void Handle()
     {
-        background.SetActive(!background.activeSelf);
+        isVisible = !isVisible;
+        if (workWithInvisableBackground)
+        {
+            background.SetActive(isVisible);
+            backgroundImage.color = new Color(1f, 1f, 1f, 1f);
+        }
+        else
+        {
+            background.SetActive(true);
+            float alpha = isVisible ? 1 : 0;
+            backgroundImage.color = new Color(1f, 1f, 1f, alpha);
+        }
     }
 }
 
